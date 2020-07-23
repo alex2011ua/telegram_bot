63bcc12e160c206656a6aa38916bc4657c267046
Aleksey Kovalenko <alex2011ua@gmail.com>
2020-07-23 13:14:09 +0300

    Telegram bot Работает!

--- bot.py
+++ bot.py
@@ -6,8 +6,9 @@ import requests
 token = '1098632551:AAFWxP9r6bQ4HTfZ54Rcau3kBAC0qMOcS00'
 bot = telebot.TeleBot(token)
 add_plase = ['добавить', 'add', 'прикрепить']
-list_plase = ['отобразить', 'показ', 'list']
-reset_plase = ['очистить', 'reset', 'удалить', 'удаление']
+list_plase = ['отобразить', 'показ', 'list', 'покажи']
+reset_plase = ['очистить', 'reset', 'удалить', 'удаление', 'стереть']
+
 START, TITLE, LOCKEYSHN, PICTCHE, CONFIRM = range(5)
 USER_STATE = defaultdict(lambda: START)
 
@@ -111,20 +112,11 @@ def check_reset(message):
 @bot.message_handler(func = check_list)
 def handle_message_list(message):
     print(message.text)
+
     chat_id = message.chat.id
     list_chat = USER_PPLACE[chat_id]
     bot.send_message(chat_id = message.chat.id,
-                     text = "Вывод списка мест:")
-    for title, value in list_chat.items():
-        text = title
-        chat_id = message.chat.id
-        lok = value[0]
-        photo = value[1]
-        a = lok.longitude
-        b = lok.latitude
-
-        bot.send_message(chat_id = chat_id, text = text)
-        bot.send_location(chat_id, b, a)
+                     text = "Укажите текущую геолокацию для поиска мест в радиусе 500 м.")
 
 
 @bot.message_handler(commands = ['reset'])
@@ -133,20 +125,28 @@ def handle_message_reset(message):
     print(message.text)
     chat_id = message.chat.id
     reset(chat_id)
-    bot.send_message(chat_id = message.chat.id, text = "Удаление всей информации")
+    bot.send_message(chat_id = message.chat.id, text = "Все сохраненные Вами места удалены!")
 
 @bot.message_handler(commands = ['add'])
 @bot.message_handler(func = check_add)
 def handle_message_add(message):
     print(message.text)
     set_state(message, TITLE)
-    bot.send_message(chat_id = message.chat.id, text = "Добавление места в память. Название места!")
-
+    bot.send_message(chat_id = message.chat.id, text = "Добавление места в память.")
+    bot.send_message(chat_id = message.chat.id, text = "Введите название этого места!")
 
 
 @bot.message_handler(func = lambda message: get_state(message) == START)
 def handle_message_start(message):
-    print(message.text)
+    bot.send_message(chat_id = message.chat.id, text = " Здравствуйте!")
+    bot.send_message(chat_id = message.chat.id, text = "Я - бот,который позволит сохранять места для будущего посещения")
+    bot.send_message(chat_id = message.chat.id,
+                     text = "Могу сохранить описание, локацию и фото необходимого места")
+    bot.send_message(chat_id = message.chat.id,
+                     text = "Могу показать все сохраненные места в радицсе 500м.")
+    bot.send_message(chat_id = message.chat.id,
+                     text = "Могу стереть все сохраненные Ваши записи.")
+
     keyboard = create_keyboard()
     bot.send_message(chat_id = message.chat.id,
                      text = "Что нужно сделать",
@@ -158,7 +158,9 @@ def handle_message_title(message):
     print(message.text)
     update_lok(message.chat.id, 'title', message.text)
     bot.send_message(chat_id = message.chat.id,
-                     text = "Добавление места в память. Загрузи локацию!")
+                     text = "Загрузи локацию!")
+    bot.send_message(chat_id = message.chat.id,
+                     text = "(нажать 'скрепку', затем зелёненькая 'Location')")
     set_state(message, LOCKEYSHN)
 
 
@@ -173,7 +175,7 @@ def handle_message_lok(message):
     bot.send_message(chat_id = message.chat.id,
                      text = text)
     bot.send_message(chat_id = message.chat.id,
-                     text = "Добавление места в память. Загрузи фото!")
+                     text = "Загрузи фото!")
     set_state(message, PICTCHE)
 
 
@@ -193,14 +195,12 @@ def handle_message_photo(message):
     except Exception as e:
         bot.reply_to(message, e)
 
-    bot.reply_to(message, "Фото добавлено, все правильно?")
-    image = open(src, 'rb')
-    bot.send_photo(message.chat.id, image, caption = 'test')
+    bot.reply_to(message, "Сохранить запись?")
+
     update_lok(message.chat.id, 'photo', src)
 
     pl = get_place(message.chat.id)
-    bot.send_message(chat_id = message.chat.id,
-                     text = 'All - {}'.format(pl))
+
     set_state(message, CONFIRM)
 
 
@@ -209,13 +209,40 @@ def handle_message_photo(message):
 @bot.message_handler(func = lambda message: get_state(message) == CONFIRM)
 def handle_message_confirm(message):
     print(message.location)
-    if "da" in message.text.lower():
+    if "da" or 'yes' or 'да' or 'ok' or 'ок' in message.text.lower():
         bot.send_message(chat_id = message.chat.id,
                          text = 'Добавлено')
         pl = get_place(message.chat.id)
         set_user_place(message.chat.id, pl['title'][0], pl['lok'], pl['photo'])
         set_state(message, START)
 
+@bot.message_handler(func = lambda message: get_state(message) == START,
+                     content_types = 'location')
+def handle_message_lok(message):
+    lat = message.location.latitude
+    lon = message.location.longitude
+    local_coord = str(lat) + ',' + str(lon)
+
+    chat_id = message.chat.id
+    list_chat = USER_PPLACE[chat_id]
+    count = 0
+    for title, value in list_chat.items():
+        text = title
+        chat_id = message.chat.id
+        lok = value[0]
+        adres, dist = google(local_coord, lok)
+        if int(dist['value']) <= 500:
+            count += 1
+            photo = value[1]
+            image = open(photo[0], 'rb')
+            a, b = lok[0].split(',')
+            bot.send_message(chat_id = chat_id, text = text)
+            bot.send_location(chat_id, a, b)
+            bot.send_photo(chat_id, image)
+    if count == 0:
+        bot.send_message(chat_id = chat_id, text = 'Рядом не найдено ни одного места!')
+    else:
+        bot.send_message(chat_id = chat_id, text = 'Количество найденых рядом мест: {}!'.format(count))
 
 if __name__ == '__main__':
     bot.polling()
