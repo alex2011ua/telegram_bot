b96d0a816ccb69c14118a03c613fc62ccfd239ee
Aleksii Kovalenko <alex2011ua@gmail.com>
2021-07-01 14:20:49 +0200

    library

--- library/urls.py
+++ library/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 
 
 urlpatterns = [
+    path('', include('author.urls')),
     path('admin/', admin.site.urls),
     path('user/', include('authentication.urls')),
     path('order/', include('order.urls')),
